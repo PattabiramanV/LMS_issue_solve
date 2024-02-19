fb593a6f593fb4645cc5363643e8a8d37c1f647c
DCKAP <santhoshkumar.edckap@gmail.com>
2024-02-19 12:23:39 +0530

    add change

---
+++ JS/home.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let home=document.querySelector(".nav_home")
+console.log(home)
+let learning=document.querySelector(".nav_learning")
+let dashboard=document.querySelector(".nav_dashboard")
+let roadmap=document.querySelector(".nav_roadmap")
+let navlogin=document.querySelector(".login")
+let navsign=document.querySelector(".sign_up")
+let searchbtn=document.querySelector(".explore")
+let htmlbtn=document.querySelector(".html_btn")
+let cssbtn=document.querySelector(".css_btn")
+let jsbtn=document.querySelector(".js_btn")
+let mysqlbtn=document.querySelector(".mysql_btn")
+let phpbtn=document.querySelector(".php_btn")
+
+
+addEventListener("")
+

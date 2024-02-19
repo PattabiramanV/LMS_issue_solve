6d382de28cbb495bf40297095de9bd9e7aa5822f
madhankumarp1 <madhankumarpdckap@gmail.com>
2024-02-19 09:26:47 +0530

    add profile js

--- JS/index.js
+++ JS/index.js
@@ -20,7 +20,115 @@
 
 //   import {  getFirestore, getDoc, getDocs, doc, setDoc, updateDoc, addDoc,  collection  } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// // -------------------------------------------------------
+
+
+//----------------------------------- navbar_sidebar
+
+
+
+
+const body = document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
+});
+
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
+
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI;
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+
+
+// Profile
+
+let profile_Dropdown = document.querySelector(".profile_bar_list");
+let profile_navigate = document.querySelector(".profile");
+
+profile_navigate.addEventListener("click", (event) => {
+    event.stopPropagation();
+    profile_Dropdown.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+    if (!profile_navigate.contains(event.target) && !profile_Dropdown.contains(event.target)) {
+        profile_Dropdown.style.display = "none"; 
+    }
+});
+
+// profile_drop
+
+let profile_page=document.querySelector(".profile_down")
+ profile_page.addEventListener("click",()=>{
+        window.location.href="/profile_user_page/profile.html"
+})
+
+let Course_navigate=document.querySelector(".Course_Down")
+Course_navigate.addEventListener("click",()=>{
+      window.location.href="/Profile_main_page/Courses.html"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ---------------------Quiz----------------------------------
 let cssquizData = [
     {
         question: '1.What does CSS stand for?',

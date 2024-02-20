7be1d780b5aef46a9ecf43fd9e05bd8127ee9827
DCKAP <pattabiramanvdckap@gmail.com>
2024-02-20 17:28:47 +0530

    learning changes added

---
+++ JS/learning.js
@@ -0,0 +1,273 @@
+"use strict"
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDB-XQdiHjT82q_r5MVNFgpyUsaU2WMvik",
+    authDomain: "dckap-lms-project.firebaseapp.com",
+    projectId: "dckap-lms-project",
+    storageBucket: "dckap-lms-project.appspot.com",
+    messagingSenderId: "1022626638467",
+    appId: "1:1022626638467:web:2c8f79d5614281ac7b49b6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  import { getFirestore,getDoc,getDocs,setDoc,updateDoc,doc,addDoc,collection,deleteDoc, deleteField } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+let db=getFirestore(app);
+let find_language=0;
+
+
+
+let all_learn_more_btn=document.querySelectorAll(".learn_more_btn");
+
+all_learn_more_btn.forEach((btn,index)=>{
+
+    btn.addEventListener("click",()=>{
+if(index==0){
+console.log("pattabi");
+    window.location.href='learning_content.html';
+}
+    })
+});
+
+// all_learn_more_btn[0].addEventListener("click",async()=>{
+
+//     let ref_data=collection(db,"Learning");
+//     let data_set=await addDoc(
+//         ref_data,{
+//             Find_Language_type:find_language
+//         }
+//     ).then(()=>{
+//         alert("sucessfully");
+//     }).catch((err)=>{
+//         console.log(err);
+//     });
+//     // window.location.href='/DCKAP_LMS/Learn_study_page/learning_content.html/';
+// })
+
+
+let Quiz_object={
+
+
+HTML:{
+
+    headers:[
+    {
+        Question:'1.What is Full form of HTML?',
+        Options: ['Hyper Text Markup Language','Hyper Text Makeup Language',
+        'Hyper Text Mark Language','Hyper Test Markup Language'],
+        Answers:['Hyper Text Markup Language','<href>',
+        'It defines a header for the document.','<table>','<body>']
+    
+    },
+
+    {
+        Question:'2.Which tag is used to create a hyperlink in HTML?',
+        Options:['<link>','<a>','<href>','hyper']
+     
+    },
+    {
+        Question:`3.In HTML, what is the purpose of the &lt;head&gt; element?`,
+        Options:['It contains the main content of the document.','It defines a header for the document.',
+        'It provides metadata about the document.','It creates a navigation bar.' ]
+ 
+    }
+    ],
+
+    form:[
+
+        {
+            Question:'1.What is Full form of HTML?',
+            Options: ['Hyper Text Markup Language','Hyper Text Makeup Language',
+            'Hyper Text Mark Language','Hyper Test Markup Language'],
+            Answers:['Hyper Text Markup Language','<href>',
+            'It defines a header for the document.','<table>','<body>']
+        
+        },
+    
+        {
+            Question:'2.Which tag is used to create a hyperlink in HTML?',
+            Options:['<link>','<a>','<href>','hyper']
+         
+        },
+        {
+            Question:`3.In HTML, what is the purpose of the &lt;head&gt; element?`,
+            Options:['It contains the main content of the document.','It defines a header for the document.',
+            'It provides metadata about the document.','It creates a navigation bar.' ]
+     
+        }
+    ]
+},
+
+CSS:{
+
+    felx:[
+        {
+            Question:'1.What is Full form of HTML?',
+            Options: ['Hyper Text Markup Language','Hyper Text Makeup Language',
+            'Hyper Text Mark Language','Hyper Test Markup Language'],
+            Answers:['Hyper Text Markup Language','<href>',
+            'It defines a header for the document.','<table>','<body>']
+        
+        },
+    
+        {
+            Question:'2.Which tag is used to create a hyperlink in HTML?',
+            Options:['<link>','<a>','<href>','hyper']
+         
+        },
+        {
+            Question:`3.In HTML, what is the purpose of the &lt;head&gt; element?`,
+            Options:['It contains the main content of the document.','It defines a header for the document.',
+            'It provides metadata about the document.','It creates a navigation bar.' ]
+     
+        }
+        ],
+    
+        Grid:[
+    
+            {
+                Question:'1.What is Full form of HTML?',
+                Options: ['Hyper Text Markup Language','Hyper Text Makeup Language',
+                'Hyper Text Mark Language','Hyper Test Markup Language'],
+                Answers:['Hyper Text Markup Language','<href>',
+                'It defines a header for the document.','<table>','<body>']
+            
+            },
+        
+            {
+                Question:'2.Which tag is used to create a hyperlink in HTML?',
+                Options:['<link>','<a>','<href>','hyper']
+             
+            },
+            {
+                Question:`3.In HTML, what is the purpose of the &lt;head&gt; element?`,
+                Options:['It contains the main content of the document.','It defines a header for the document.',
+                'It provides metadata about the document.','It creates a navigation bar.' ]
+         
+            }
+        ]
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//................................. Nav bar...................................//
+
+
+const body = document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
+});
+
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
+
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI;
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+
+
+// Profile
+
+let profile_Dropdown = document.querySelector(".profile_bar_list");
+let profile_navigate = document.querySelector(".profile");
+
+profile_navigate.addEventListener("click", (event) => {
+    event.stopPropagation();
+    profile_Dropdown.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+    if (!profile_navigate.contains(event.target) && !profile_Dropdown.contains(event.target)) {
+        profile_Dropdown.style.display = "none"; 
+    }
+});
+
+// profile_drop
+
+let profile_page=document.querySelector(".profile_down")
+ profile_page.addEventListener("click",()=>{
+        window.location.href="/profile_user_page/profile.html"
+})
+
+let Course_navigate=document.querySelector(".Course_Down")
+Course_navigate.addEventListener("click",()=>{
+      window.location.href="/Profile_main_page/Courses.html"
+})
---
+++ JS/learning_content.js
@@ -0,0 +1,228 @@
+"use strict";
+
+//................................. Nav bar...................................//
+
+
+const body = document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
+});
+
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
+
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI;
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+
+
+// Profile
+
+let profile_Dropdown = document.querySelector(".profile_bar_list");
+let profile_navigate = document.querySelector(".profile");
+
+profile_navigate.addEventListener("click", (event) => {
+    event.stopPropagation();
+    profile_Dropdown.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+    if (!profile_navigate.contains(event.target) && !profile_Dropdown.contains(event.target)) {
+        profile_Dropdown.style.display = "none"; 
+    }
+});
+
+// profile_drop
+
+let profile_page=document.querySelector(".profile_down")
+ profile_page.addEventListener("click",()=>{
+        window.location.href="/profile_user_page/profile.html"
+})
+
+let Course_navigate=document.querySelector(".Course_Down")
+Course_navigate.addEventListener("click",()=>{
+      window.location.href="/Profile_main_page/Courses.html"
+})
+
+
+
+//.....................................Content_JS...................................//
+
+
+
+
+let quiz_btn=document.querySelector(".quiz_btn");
+console.log(quiz_btn);
+quiz_btn.addEventListener("click",()=>{
+console.log('pattabi');
+    window.location.href='learning_quiz.html';
+})
+
+
+
+let content=[
+
+    
+    [
+
+        "&gt;!DOCTYPE html&lt;"+
+    "<html lang=\"en\">"+
+    "<head>"+
+    "  <title>Title of the document</title>"+
+    "</head>"+
+    "<body>"+
+    "<h1>This is a heading</h1>"+
+    "<p>This is a paragraph.</p>"+
+   
+    "</body>"+
+    "</html>",
+ '"https://www.youtube.com/embed/Y7S9Z26Q6tQ?si=bmK9eyMR5fpBUW8w" ',
+ 'HTML stands for Hyper Text Markup Language <br>HTML is the standard markup <b>'+
+  'language for creating Web pages <br> HTML describes the structure of a <b>'+
+  'Web page<br>HTML consists of a series of elements <br>'+
+ 'HTML elements tell the browser how to display the content <br>'+
+ 'HTML elements label pieces of content such as "this is a heading", "this is'+
+ ' a paragraph", "this is a link", etc.<br>'
+
+    ],
+
+    
+   [
+    `
+        <h2 class=heading_tag> HTML Attributes:</h2>
+    
+        All HTML elements can have attributes
+    
+        Attributes provide additional information about elements <br>
+    
+        Attributes are always specified in the start tag <br>
+    
+         Attributes usually come in name/value pairs like: <b>name="value"</b>
+        
+        <h2 class=heading_tag>The href Attribute: </h2>
+    
+        The <span class=red_color>&lt;a&gt</span> tag defines a hyperlink. The <span class=red_color>href</span> attribute specifies the URL of the page the link goes to:
+    
+        <h2 class=heading_tag>Example:</h2>
+       &lt;!DOCTYPE html&gt;<br>
+        &lt;html&gt; <br>
+        &lt;body&gt;<br>
+        
+        &lt;h2&gt;The href Attribute&lt;/h2&gt<br>
+        
+        &lt;p&gt;HTML links are defined with the a tag. The link address is specified in the href attribute:&lt;/p&gt;<br>
+        
+        &lt;a <span class=red_color>href</span>="https://www.w3schools.com"&gt;Visit W3Schools&lt;/a&gt;<br>
+        
+        &lt;/body&gt;<br>
+      
+        &lt;/html&gt;
+        
+        
+        The src Attribute
+        The <img> tag is used to embed an image in an HTML page. The src attribute specifies the path to the image to be displayed:
+        Example
+        <img src="img_girl.jpg">
+        
+        The alt Attribute
+        The required alt attribute for the <img> tag specifies an alternate text for an image, if the image for some reason cannot be displayed. This can be due to a slow connection, or an error in the src attribute, or if the user uses a screen reader.
+        Example
+        <img src="img_girl.jpg" alt="Girl with a jacket">
+        
+        The: style Attribute
+        The style attribute is used to add styles to an element, such as color, font, size, and more.
+         Example:
+        <p style="color:red;">This is a red paragraph.</p>`,  
+        `https://www.youtube.com/embed/j2DhhN-_2Ww?si=t7Yk1ir56zjm_bdq`
+    
+    
+       ]
+       
+    
+]
+
+
+let all_heading_title=document.querySelectorAll(".content_title");
+
+
+let content_top=document.querySelector(".content_top");
+
+let iframe=document.querySelector("iframe");
+
+let bottom_content=document.querySelector(".bottom_content1");
+
+// all_heading_title.forEach((heading,index)=>{
+
+// heading.addEventListener("click",()=>{
+ 
+content_top.innerHTML=content[1][0];
+iframe.src=content[1][1];
+bottom_content.innerHTML=arr[0]
+
+// });
+
+// });
+
+// all_heading_title[0].addEventListener("click",()=>{
+
+
+// content_top.innerHTML=content[1][0];
+// iframe.src=content[1][1];
+// bottom_content.innerText=content[1][2]
+
+// })
+
+document.querySelector("#intoduction").addEventListener("click",()=>{
+    console.log("pattabi");
+});
+
+let arr=[
+  `pattabiiiiiiiiiiii
+  raman nnnnnnnnnnnnn
+  `
+]
+console.log(arr[0]);
+bottom_content.innerHTML=arr[0]

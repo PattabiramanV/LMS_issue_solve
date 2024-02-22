b3e176c443a3ae152a33785003eae5f816c15ec5
DCKAP <varshithaksdckap@gmail.com>
2024-02-22 15:45:16 +0530

    modifing the code

--- JS/certificate.js
+++ JS/certificate.js
@@ -100,19 +100,34 @@ Course_navigate.addEventListener("click",()=>{
 
 // --------------- CSS Quiz -------------------
 
+const html_btn =document.querySelector("#html_btn");
+html_btn.addEventListener("click", () => {
+  localStorage.setItem('selectedQuiz', 'HTML_Overall_Quiz');
+  window.location.href = 'OverallQuiz.html';
+});
+
+
 const css_quiz = document.querySelector("#css_btn");
 css_quiz.addEventListener("click", () => {
   // Store the selected quiz type in localStorage
   localStorage.setItem('selectedQuiz', 'CSS_Overall_Quiz');
-  // localStorage.setItem('selectedQuiz', 'HTML_Overall_Quiz');
-  // localStorage.setItem('selectedQuiz', 'JavaScript_Overall_Quiz');
-  // localStorage.setItem('selectedQuiz', 'PHP_Overall_Quiz');
-  // localStorage.setItem('selectedQuiz', 'MySql_Overall_Quiz');
 
 
   // console.log('CSS quiz button clicked');
   // console.log('selectedQuiz in localStorage:', localStorage.getItem('selectedQuiz'));
 
-  window.location.href = 'index.html';
+  window.location.href = 'OverallQuiz.html';
 
+});
+
+const js_btn =document.querySelector("#js_btn");
+js_btn.addEventListener("click", () => {
+  localStorage.setItem('selectedQuiz', 'JavaScript_Overall_Quiz');
+  window.location.href = 'OverallQuiz.html';
+});
+
+const mysql_btn =document.querySelector("#mysql_btn");
+mysql_btn.addEventListener("click", () => {
+  localStorage.setItem('selectedQuiz', 'MySql_Overall_Quiz');
+  window.location.href = 'OverallQuiz.html';
 });
